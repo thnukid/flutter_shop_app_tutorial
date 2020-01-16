3ac163db8211c0847ea0512dd2bcf4c4f89fec51
Eric Bartholemy <eric@kabisa.nl>
2020-01-16 17:46:34 +0100

    198. Local State vs App-wide State (2) with Widget State

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,28 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
-import '../providers/products.dart';
 
 enum FilterOptions {
   Favorites,
   All,
 }
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
-    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
         appBar: AppBar(
           title: Text('My Shop'),
           actions: <Widget>[
             PopupMenuButton(
               onSelected: (FilterOptions selectedValue) {
-                if (selectedValue == FilterOptions.Favorites) {
-                  productsContainer.showFavoritesOnly();
-                } else {
-                  productsContainer.showAll();
-                }
+                setState(() {
+                  if (selectedValue == FilterOptions.Favorites) {
+                    _showOnlyFavorites = true;
+                  } else {
+                    _showOnlyFavorites = false;
+                  }
+                });
               },
               icon: Icon(Icons.more_vert),
               itemBuilder: (_) => [
@@ -33,6 +40,6 @@ class ProductsOverviewScreen extends StatelessWidget {
             )
           ],
         ),
-        body: ProductsGrid());
+        body: ProductsGrid(_showOnlyFavorites));
   }
 }
