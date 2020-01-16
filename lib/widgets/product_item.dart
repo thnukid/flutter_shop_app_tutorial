0f3cd82ecee3e5851830ee56aabd50d44db96968
Eric Bartholemy <eric@kabisa.nl>
2020-01-16 12:51:48 +0100

    191. Working with Providers & Listeners

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -15,9 +15,10 @@ class ProductItem extends StatelessWidget {
       child: GridTile(
           child: GestureDetector(
             onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => ProductDetailScreen(title)
-              ));
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: id,
+              );
             },
             child: Image.network(imageUrl, fit: BoxFit.cover),
           ),
